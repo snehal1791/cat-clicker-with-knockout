b9bba465943316e9322888d9dde478d1c39a55ce
Snehal Parate <sparate@my.harrisburgu.edu>
2016-12-03 23:13:11 -0500

    Add multiple cats

--- js/app.js
+++ js/app.js
@@ -1,13 +1,50 @@
-var ViewModel = function() {
-    this.clickCount = ko.observable(0);
-    this.name = ko.observable('Tabby');
-    this.imgSrc = ko.observable('img/4154543904_6e2428c421_z.jpg');
-    this.imgAttribution = ko.observable('Flickr');
-    this.age = ko.observable();
+var initialCats = [
+    {
+        clickCount: 0,
+        name: 'Tabby',
+        imgSrc: 'img/4154543904_6e2428c421_z.jpg',
+        imgAttribution: 'Flickr',
+        nicknames: ['Smiley']
+    },
+    {
+        clickCount: 0,
+        name: 'Kitty',
+        imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+        imgAttribution: 'Flickr',
+        nicknames: ['Starey']
+    },
+    {
+        clickCount: 0,
+        name: 'Whitey',
+        imgSrc: 'img/22252709_010df3379e_z.jpg',
+        imgAttribution: 'Flickr',
+        nicknames: ['Snowy']
+    },
+    {
+        clickCount: 0,
+        name: 'Sofie',
+        imgSrc: 'img/1413379559_412a540d29_z.jpg',
+        imgAttribution: 'Flickr',
+        nicknames: ['Fatso']
+    },
+    {
+        clickCount: 0,
+        name: 'Rosie',
+        imgSrc: 'img/9648464288_2516b35537_z.jpg',
+        imgAttribution: 'Flickr',
+        nicknames: ['Tabtab']
+    }
+];
 
-    this.incrementCounter = function() {
-        this.clickCount(this.clickCount() + 1);
-    };
+var Cat = function(data){
+    this.clickCount = ko.observable(data.clickCount);
+    this.name = ko.observable(data.name);
+    this.imgSrc = ko.observable(data.imgSrc);
+    this.imgAttribution = ko.observable(data.imgAttribution);
+    this.age = ko.observable();
+    //observable array
+    this.nicknames = ko.observableArray(data.nicknames);
+
     this.showLevel = ko.computed(function() {
         if (this.clickCount() >= 5 & this.clickCount() <= 10){
             return this.age('Infant');
@@ -19,6 +56,33 @@ var ViewModel = function() {
             return this.age('Just Born');
         }
     }, this);
+}
+
+var ViewModel = function() {
+
+    var self = this;
+
+    this.catList = ko.observableArray([]);
+
+    initialCats.forEach(function(catItem){
+        self.catList.push( new Cat(catItem));
+    });
+
+    this.currentCat = ko.observable(this.catList()[0]);
+
+    this.newCat = function(catData){
+        self.currentCat(catData);
+    };
+
+    this.incrementCounter = function() {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };
+
+    // or use this methos. here self = this refers to ViewModel
+    /*var self = this;
+    this.incrementCounter = function() {
+        self.currentCat().clickCount(self.currentCat().clickCount() + 1);
+    };*/
 };
 
 ko.applyBindings(new ViewModel());
